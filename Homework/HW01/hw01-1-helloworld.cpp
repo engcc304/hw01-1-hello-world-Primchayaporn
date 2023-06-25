126250a2e6bc372882f8fb0961a35b30d033602d
Ploy Primchayaporn <137378257+Primchayaporn@users.noreply.github.com>
2023-06-25 16:18:47 +0700

    Update hw01-1-helloworld.cpp

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,5 +1,11 @@
 //TODO: #3 provide me full syntax of C-Language contain include, main function(int).
 
 //TODO: #4 display me "Hello World." to me.
+#include <stdio.h>
 
+int main() {
+    printf("Hello world");
+
+    return 0;
+}
 //TODO: #5 Write your private info into Student-Info.txt
